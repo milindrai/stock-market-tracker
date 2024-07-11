30693efa879561d97ec45cad8fbf5a4602f2f55c
milindrai <milindrai365@gmail.com>
2024-07-11 19:14:01 +0530

    setup db

---
+++ src/config/db.js
@@ -0,0 +1,13 @@
+
+//import .env file
+require('dotenv').config();
+
+
+const { Pool } = require('pg');
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+});
+
+module.exports = {
+  query: (text, params) => pool.query(text, params),
+};
