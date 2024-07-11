2012f627e681de66d040d3cd7be8dc68b9997296
milindrai <milindrai365@gmail.com>
2024-07-11 19:28:24 +0530

    Add cors package

--- src/server.js
+++ src/server.js
@@ -1,8 +1,17 @@
 const app = require('./app');
 const db = require('./config/db');
 require('dotenv').config();
+const cron = require('node-cron');
+const stockController = require('./controllers/stockController');
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
+});
+
+
+// Schedule the job to run every minute
+cron.schedule('* * * * *', async () => {
+  console.log('Fetching and storing stock data...');
+  await stockController.fetchAndStoreStockData();
 });
